d676b0735c178adfc297361a51509ae53a450122
aahil n <aahiltn@gmail.com>
2025-02-09 05:43:52 -0500

    feat: new .env

--- src/app/api/dynamo/client.js
+++ src/app/api/dynamo/client.js
@@ -2,10 +2,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION,
+  region: process.env.REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_AWS_KEY_ID,
+    secretAccessKey: process.env.SECRET_AWS_ACCESS_KEY,
   },
 });
 const dynamo = DynamoDBDocumentClient.from(client);
--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -6,8 +6,8 @@ import { startOfDay, endOfDay } from "date-fns";
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.ACCESS_AWS_KEY_ID!,
+    secretAccessKey: process.env.SECRET_AWS_ACCESS_KEY!,
   },
 });
 
